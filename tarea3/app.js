a8c404626406525b6e1b85828140932b023c97d2
ManuelLopezParedes <manuel.eduardo.lopeza@gmail.com>
2024-08-28 23:40:49 -0600

    funcion suma

---
+++ tarea3/app.js
@@ -0,0 +1,10 @@
+function suma(numero1, numero2){
+    return numero1 + numero2;
+    }
+
+let dato1 = parseInt(prompt("dame un numero para sumar"));
+let dato2 = parseInt(prompt("dame un segundo numero"));
+
+res=suma(dato1,dato2)
+
+console.log("el resultado es " + res)
